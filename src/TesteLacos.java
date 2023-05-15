e993368e22bbbf8f1da595cf830a198b1c68f626
Eliane Marion <pf1551@fiap.com.br>
2023-05-15 18:51:55 -0300

    Primeira versao

---
+++ src/TesteLacos.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TesteLacos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double altura1, altura2, altura3, altura4, altura5, altura6, media;
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Digite a altura do 1º aluno");
+		altura1 = leitor.nextDouble();
+		System.out.println("Digite a altura do 2º aluno");
+		altura2 = leitor.nextDouble();
+		System.out.println("Digite a altura do 3º aluno");
+		altura3 = leitor.nextDouble();
+		System.out.println("Digite a altura do 4º aluno");
+		altura4 = leitor.nextDouble();
+		System.out.println("Digite a altura do 5º aluno");
+		altura5 = leitor.nextDouble();
+		System.out.println("Digite a altura do 6º aluno");
+		altura6 = leitor.nextDouble();
+		media = (altura1 + altura2 + altura3 + altura4 + altura5 + altura6)/6;
+		System.out.println("A media de altura dos alunos é de " + media);
+		leitor.close();
+	}
+
+}
